30e1aa4e4bb070598ede82168c95df8f2125e102
mort65 <mohsenbani65@gmail.com>
2018-04-07 00:32:39 +0430

    Update config.py

--- mort65/config.py
+++ mort65/config.py
@@ -225,26 +225,22 @@ def find_or_run(app,classes=[],group="",processes=[]):
     if not processes:
         processes = [regex(app.split('/')[-1])]
     def __inner(qtile):
-        if classes or group:
-            if classes:
-                for window in qtile.windowMap.values():
-                    for c in classes:
-                        if (window.group and window.match(wmclass=c)):
-                            qtile.currentScreen.setGroup(window.group)
-                            window.group.focus(window, False)
-                            return
-            if group:
-                lines = subprocess.check_output(["/usr/bin/ps", "axw"]).decode("utf-8").splitlines()
-                ls = [line.split()[4:] for line in lines][1:]
-                ps = [' '.join(l) for l in ls]
-                is_running = False
-                for p in ps:
-                    for process in processes:
-                        if re.match(process, p):
-                            is_running = True
-                if is_running:
-                    qtile.groupMap[group].cmd_toscreen()
-                    return
+        if classes:
+            for window in qtile.windowMap.values():
+                for c in classes:
+                    if (window.group and window.match(wmclass=c)):
+                        qtile.currentScreen.setGroup(window.group)
+                        window.group.focus(window, False)
+                        return
+        if group:
+            lines = subprocess.check_output(["/usr/bin/ps", "axw"]).decode("utf-8").splitlines()
+            ls = [line.split()[4:] for line in lines][1:]
+            ps = [' '.join(l) for l in ls]
+            for p in ps:
+                for process in processes:
+                    if re.match(process, p):
+                        qtile.groupMap[group].cmd_toscreen()
+                        return
         subprocess.Popen(app.split())
     return __inner
     
