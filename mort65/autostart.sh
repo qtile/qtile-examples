f599771bca7b1ac93a80f519c6e05145469ea3a7
mort65 <mohsenbani65@gmail.com>
2018-04-07 07:45:15 +0430

    Update autostart.sh

--- mort65/autostart.sh
+++ mort65/autostart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 exec /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-exec /usr/bin/compton &
+exec /usr/bin/compton -b &
 exec /usr/bin/nm-applet &
 exec /usr/bin/volumeicon &
 exec /usr/bin/clipit &
