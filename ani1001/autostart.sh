2bfdede2c04894b62b8e830ac85a0af37f6f9312
Anik Biswas <90283502+ani1001@users.noreply.github.com>
2021-09-13 01:48:07 +0600

    Add files via upload

---
+++ ani1001/autostart.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+nitrogen --restore &
+picom &
+lxpolkit &
--- ani1001/config.py
+++ ani1001/config.py
@@ -1,1 +1,221 @@
+# Copyright (c) 2010 Aldo Cortesi
+# Copyright (c) 2010, 2014 dequis
+# Copyright (c) 2012 Randall Ma
+# Copyright (c) 2012-2014 Tycho Andersen
+# Copyright (c) 2012 Craig Barnes
+# Copyright (c) 2013 horsik
+# Copyright (c) 2013 Tao Sauvage
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
+import os
+import re
+import socket
+import subprocess
+from typing import List  # noqa: F401
+from libqtile import bar, layout, widget, hook
+from libqtile.config import Click, Drag, Group, Key, KeyChord, Screen
+from libqtile.lazy import lazy
+from libqtile.command import lazy
+
+mod = "mod4"
+myTerminal = "alacritty"
+myConfig = "~/.config/qtile/config.py"
+
+keys = [
+    # Switch between windows in current stack pane
+    Key([mod], "k", lazy.layout.down(),
+        desc="Move focus down in stack pane"),
+    Key([mod], "j", lazy.layout.up(),
+        desc="Move focus up in stack pane"),
+
+    # Move windows up or down in current stack
+    Key([mod, "control"], "k", lazy.layout.shuffle_down(),
+        desc="Move window down in current stack "),
+    Key([mod, "control"], "j", lazy.layout.shuffle_up(),
+        desc="Move window up in current stack "),
+
+    # Switch window focus to other pane(s) of stack
+    Key([mod], "space", lazy.layout.next(),
+        desc="Switch window focus to other pane(s) of stack"),
+    # Swap panes of split stack
+    Key([mod, "shift"], "space", lazy.layout.rotate(),
+        desc="Swap panes of split stack"),
+
+    # Toggle between split and unsplit sides of stack.
+    # Split = all windows displayed
+    # Unsplit = 1 window displayed, like Max layout, but still with
+    # multiple stack panes
+    Key([mod, "shift"], "Return", lazy.layout.toggle_split(),
+        desc="Toggle between split and unsplit sides of stack"),
+    Key([mod], "Return", lazy.spawn(myTerminal), desc="Launch terminal"),
+
+    # Toggle between different layouts as defined below
+    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
+
+    Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
+    Key([mod, "control"], "r", lazy.restart(), desc="Restart qtile"),
+    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown qtile"),
+
+    Key([mod], "r", lazy.spawn("rofi -show run"), desc="Launch rofi"),
+]
+
+groups = [Group(i) for i in "asdfuiop"]
+
+for i in groups:
+    keys.extend([
+        # mod1 + letter of group = switch to group
+        Key([mod], i.name, lazy.group[i.name].toscreen(),
+            desc="Switch to group {}".format(i.name)),
+
+        # mod1 + shift + letter of group = switch to & move focused window to group
+        Key([mod, "shift"], i.name, lazy.window.togroup(i.name, switch_group=True),
+            desc="Switch to & move focused window to group {}".format(i.name)),
+        # Or, use below if you prefer not to switch to that group.
+        # # mod1 + shift + letter of group = move focused window to group
+        # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
+        #     desc="move focused window to group {}".format(i.name)),
+    ])
+
+def init_layout_theme():
+    return {"margin": 4,
+            "border_width": 2,
+            "border_focus": '#5e81ac',
+            "border_normal": '#4c566a'
+            }
+
+layout_theme = init_layout_theme()
+
+layouts = [
+    # layout.Max(),
+    # layout.Stack(num_stacks=2),
+    # Try more layouts by unleashing below layouts.
+    layout.Bsp(**layout_theme),
+    # layout.Columns(),
+    layout.Matrix(**layout_theme),
+    layout.MonadTall(margin=6, border_width=2, border_focus='#5e81ac', border_normal='#4c566a'),
+    layout.MonadWide(margin=6, border_width=2, border_focus='#5e81ac', border_normal='#4c566a'),
+    layout.RatioTile(**layout_theme),
+    # layout.Tile(),
+    # layout.TreeTab(),
+    layout.VerticalTile(**layout_theme),
+    # layout.Zoomy(),
+]
+
+widget_defaults = dict(
+    font='ShureTechMono Nerd Font',
+    fontsize=14,
+    padding=3,
+    background='#2e3440',
+)
+extension_defaults = widget_defaults.copy()
+
+screens = [
+    Screen(
+        top=bar.Bar(
+            [
+                widget.GroupBox(
+                    active='#eceff4',
+                    inactive='#4c566a',
+                    highlight_method='line',
+                    this_current_screen_border='#ebcb8b'
+                    ),
+                widget.Spacer(),
+
+                widget.CPU(foreground='#88c0d0'),
+                widget.Sep(),
+
+                widget.Memory(
+                    format='{MemUsed:.0f}{mm}',
+                    foreground='#8fbcbb'
+                    ),
+                widget.Sep(),
+
+                widget.Net(foreground ='#a3be8c'),
+                widget.Sep(),
+
+                widget.Clock(
+                    format='%a %d, (%B) %H:%M',
+                    foreground='#b48ead'
+                    ),
+                widget.Sep(),
+
+                widget.CurrentLayout(
+                    font='ShureTechMono Nerd Font Bold',
+                    foreground='#5e81ac'
+                    ),
+                widget.Sep(),
+
+                widget.CurrentLayoutIcon(),
+                ],
+
+            24,
+        ),
+    ),
+]
+
+# Drag floating layouts.
+mouse = [
+    Drag([mod], "Button1", lazy.window.set_position_floating(),
+         start=lazy.window.get_position()),
+    Drag([mod], "Button3", lazy.window.set_size_floating(),
+         start=lazy.window.get_size()),
+    Click([mod], "Button2", lazy.window.bring_to_front())
+]
+
+dgroups_key_binder = None
+dgroups_app_rules = []  # type: List
+main = None  # WARNING: this is deprecated and will be removed soon
+follow_mouse_focus = True
+bring_front_click = False
+cursor_warp = False
+floating_layout = layout.Floating(float_rules=[
+    # Run the utility of `xprop` to see the wm class and name of an X client.
+    {'wmclass': 'confirm'},
+    {'wmclass': 'dialog'},
+    {'wmclass': 'download'},
+    {'wmclass': 'error'},
+    {'wmclass': 'file_progress'},
+    {'wmclass': 'notification'},
+    {'wmclass': 'splash'},
+    {'wmclass': 'toolbar'},
+    {'wmclass': 'confirmreset'},  # gitk
+    {'wmclass': 'makebranch'},  # gitk
+    {'wmclass': 'maketag'},  # gitk
+    {'wname': 'branchdialog'},  # gitk
+    {'wname': 'pinentry'},  # GPG key password entry
+    {'wmclass': 'ssh-askpass'},  # ssh-askpass
+])
+auto_fullscreen = True
+focus_on_window_activation = "smart"
+
+@hook.subscribe.startup_once
+def autostart():
+    home = os.path.expanduser('~')
+    subprocess.Popen([home + '/.config/qtile/autostart.sh'])
+
+# XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
+# string besides java UI toolkits; you can see several discussions on the
+# mailing lists, GitHub issues, and other WM documentation that suggest setting
+# this string if your java app doesn't work correctly. We may as well just lie
+# and say that we're a working one by default.
+#
+# We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
+# java that happens to be on java's whitelist.
+wmname = "LG3D"
