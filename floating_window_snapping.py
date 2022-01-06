06717770f0da194fe4f9a8d1bd43363684681824
yobleck <sennewell@gmail.com>
2022-01-05 19:05:54 -0500

    moved helper functions to main scope

--- floating_window_snapping.py
+++ floating_window_snapping.py
@@ -24,51 +24,53 @@ TODO: border snapping on floating window resize
 from libqtile.lazy import lazy
 
 
+def _get_borders(window):
+    """Generate list of 4-tuples describing
+    the borders of every window and screen.
+    """
+    borders = []
+    for s in window.qtile.screens:
+        borders.append((s.x, s.y, s.x + s.width, s.y + s.height))
+        for w in s.group.windows:
+            if not w.hidden:
+                borders.append(w.edges)
+    borders.remove(window.edges)
+    return borders
+
+
+def _borders_touch(window, x, y, snap_dist):
+    """Compares this window's borders to the borders of other
+    windows/screens to see if they touch.
+    """
+    overlap_args = {"x": x, "y": y}
+    borders = _get_borders(window)
+    for b in borders:
+        # Are the two borders on the same line
+        if any(i in [window.edges[0], window.edges[2]] for i in [b[0], b[2]]):
+            # Are they actually overlapping
+            if window.edges[1] < b[3] and window.edges[3] > b[1]:
+                # Has the mouse moved outside of the snap area
+                if any(abs(window.edges[i] - x) < snap_dist for i in [0, 2]):
+                    try:
+                        # Window should snap so don't move along this axis
+                        del overlap_args["x"]
+                    except Exception:
+                        pass
+        # Repeat for y
+        if any(i in [window.edges[1], window.edges[3]] for i in [b[1], b[3]]):
+            if window.edges[0] < b[2] and window.edges[2] > b[0]:
+                if any(abs(window.edges[i] - y) < snap_dist for i in [1, 3]):
+                    try:
+                        del overlap_args["y"]
+                    except Exception:
+                        pass
+    return overlap_args
+
+
 @lazy.window.function
 def move_snap_window(window, x, y, snap_dist=20):
     """Move floating window to x and y.
     Border snapping makes floating window's borders
     stick to other borders for easy alignment
     """
-    def _get_borders(window):
-        """Generate list of 4-tuples describing
-        the borders of every window and screen.
-        """
-        borders = []
-        for s in window.qtile.screens:
-            borders.append((s.x, s.y, s.x + s.width, s.y + s.height))
-            for w in s.group.windows:
-                if not w.hidden:
-                    borders.append(w.edges)
-        borders.remove(window.edges)
-        return borders
-
-    def _borders_touch(window, x, y, snap_dist):
-        """Compares this window's borders to the borders of other
-        windows/screens to see if they touch.
-        """
-        overlap_args = {"x": x, "y": y}
-        borders = _get_borders(window)
-        for b in borders:
-            # Are the two borders on the same line
-            if any(i in [window.edges[0], window.edges[2]] for i in [b[0], b[2]]):
-                # Are they actually overlapping
-                if window.edges[1] < b[3] and window.edges[3] > b[1]:
-                    # Has the mouse moved outside of the snap area
-                    if any(abs(window.edges[i] - x) < snap_dist for i in [0, 2]):
-                        try:
-                            # Window should snap so don't move along this axis
-                            del overlap_args["x"]
-                        except Exception:
-                            pass
-            # Repeat for y
-            if any(i in [window.edges[1], window.edges[3]] for i in [b[1], b[3]]):
-                if window.edges[0] < b[2] and window.edges[2] > b[0]:
-                    if any(abs(window.edges[i] - y) < snap_dist for i in [1, 3]):
-                        try:
-                            del overlap_args["y"]
-                        except Exception:
-                            pass
-        return overlap_args
-
     window.tweak_float(**_borders_touch(window, x, y, snap_dist)) 
