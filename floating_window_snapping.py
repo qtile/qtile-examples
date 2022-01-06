7c8b32677c688fe67b4995ac25e228d3a62d39f9
yobleck <sennewell@gmail.com>
2022-01-05 21:02:37 -0500

    now takes into account border width

--- floating_window_snapping.py
+++ floating_window_snapping.py
@@ -26,15 +26,15 @@ from libqtile.lazy import lazy
 
 def _get_borders(window):
     """Generate list of 4-tuples describing
-    the borders of every window and screen.
+    the borders including border width of every window and screen.
     """
     borders = []
     for s in window.qtile.screens:
         borders.append((s.x, s.y, s.x + s.width, s.y + s.height))
         for w in s.group.windows:
             if not w.hidden:
-                borders.append(w.edges)
-    borders.remove(window.edges)
+                borders.append((w.x, w.y, w.x+w.width+2*w.borderwidth, w.y+w.height+2*w.borderwidth))
+    borders.remove((window.x, window.y, window.x+window.width+2*window.borderwidth, window.y+window.height+2*window.borderwidth))
     return borders
 
 
@@ -46,7 +46,7 @@ def _borders_touch(window, x, y, snap_dist):
     borders = _get_borders(window)
     for b in borders:
         # Are the two borders on the same line
-        if any(i in [window.edges[0], window.edges[2]] for i in [b[0], b[2]]):
+        if any(i in [window.edges[0], window.edges[2]+2*window.borderwidth] for i in [b[0], b[2]]):
             # Are they actually overlapping
             if window.edges[1] < b[3] and window.edges[3] > b[1]:
                 # Has the mouse moved outside of the snap area
@@ -57,7 +57,7 @@ def _borders_touch(window, x, y, snap_dist):
                     except Exception:
                         pass
         # Repeat for y
-        if any(i in [window.edges[1], window.edges[3]] for i in [b[1], b[3]]):
+        if any(i in [window.edges[1], window.edges[3]+2*window.borderwidth] for i in [b[1], b[3]]):
             if window.edges[0] < b[2] and window.edges[2] > b[0]:
                 if any(abs(window.edges[i] - y) < snap_dist for i in [1, 3]):
                     try:
